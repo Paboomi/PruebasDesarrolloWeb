393b867bbabb5e22437a551058c50a7405ef1cd9
Paboomi <githubpaboomi@gmail.com>
2024-06-23 21:02:30 -0600

    Nuevo Proyecto añadido

---
+++ frontend/src/components/Router/Router.jsx
@@ -0,0 +1,19 @@
+//Librerias para utilizar Router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "../Login";
+//import Admin from "../Admin";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Creación de las distintas rutas que se tendrán (urls) y el componente que se llamará */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        {/*<Route path="/admin" element={<Admin />} />*/}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+//import React from "react";
+import ReactDOM from "react-dom/client";
+//import { App } from "./App.jsx";
+//import "./index.css";
+//import Login from "./components/Login.jsx";
+import Router from "./components/Router/Router.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Router />);
